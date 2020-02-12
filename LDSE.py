6211747d16b5d7b8bd51f52df19e7fda9bb7f0c3
Matheus Fauth <matheuscfauth@gmail.com>
2020-02-12 11:22:20 -0300

    as

--- LDSE.py
+++ LDSE.py
@@ -1,56 +1,79 @@
 class No():
-    def __init__(self,valor,prox):
+    def __init__(self,valor,proximo):
         self.info=valor
-        self.prox=prox
-class LDSE():
+        self.prox=proximo
+        
+class Ldse():
     def __init__(self):
         self.prim=self.ult=None
         self.quant=0
+
     def inserirInicio(self,valor):
         if self.quant==0:
             self.prim=self.ult=No(valor,None)
         else:
             self.prim=No(valor,self.prim)
         self.quant+=1
-    def removerInicio(self):
-        if self.quant == 1:
-            self.prim=self.ult=None
-        else:
-            self.prim=self.prim.prox
-        self.quant-=1
-    def show(self):
-        aux=self.prim
-        while aux!=None:
-            print(aux.info)
-            aux=aux.prox
+
     def inserirFim(self,valor):
+        aux=No(valor,None)
         if self.quant==0:
             self.prim=self.ult=No(valor,None)
         else:
             self.ult.prox=self.ult=No(valor,None)
         self.quant+=1
+
+    def show(self):
+        aux=self.prim
+        while aux!=None:
+            print(aux.info)
+            aux=aux.prox
+
+    def removerInicio(self):
+        if self.quant==0:
+            self.prim=self.ult=None
+        else:
+            self.prim=self.prim.prox
+        self.quant-=1
+
     def removerFim(self):
-        if self.quant == 1:
+        if self.quant==1:
             self.prim=self.ult=None
         else:
             aux=self.prim
-            while aux.prox!=self.ult:
+            while aux.prox != self.ult:
                 aux=aux.prox
-            self.ult=aux
+            self.ult=self.aux
             self.ult.prox=None
         self.quant-=1
-    def inserirAntesDe(self,valor1,valor2):
-        ant=aux=self.prim
-        if self.prim.info==valor2:
-            self.prim=No(valor1,self.prim)
-            aux=aux.prox
+
+    def getPrim(self):
+        return self.prim.info
+
+    def getUlt(self):
+        aux=self.ult
+        return self.ult.info
+
+    def getQuant(self):
+        return self.quant
+
+    def estahVazia(self):
+        return self.quant==0
+
+    def inserirApos(self,valor1,valor2):
+        aux1=aux2=self.prim
+        if self.prim.info == valor1:
+            aux2=No(valor2,self.prim.prox)
+            self.prim.prox=aux2
             self.quant+=1
-        while aux!=None:
-            if aux.info==valor2:
-                ant.prox=No(valor1,aux)
-                self.quant+=1
-            ant=aux
-            aux=aux.prox
-        
-        
-                
+        else:
+            while aux1.info != valor1:
+                aux1=aux1.prox
+            if aux1.info == valor1 and aux1.prox != None:
+                aux2=No(valor2,aux1.prox)
+                aux1.prox=aux2
+            else:
+                aux2=No(valor2,None)
+                self.ult.prox=aux2
+                self.ult=aux2
+            self.quant+=1
--- LES.py
+++ LES.py
@@ -1,87 +1,74 @@
-class LES():
+class Les():
     def __init__(self):
-        self.list=[None,None,None,None,None]
+        self.vet=[None,None,None,None,None]
         self.quant=0
-    def estaVazio(self):
+        
+    def estahvazia(self):
         return self.quant==0
-    def estaCheia(self):
+    
+    def estahcheia(self):
         return self.quant==5
-    def getQuant(self):
+    
+    def pegaQuant(self):
         return self.quant
-    def insertEnd(self,value):
-        self.list[self.quant]=value
+    
+    def inserirFim(self,valor):
+        self.vet[self.quant]=valor
         self.quant+=1
+        
     def show(self):
         i=0
         while i<self.quant:
-            print(self.list[i])
+            print(self.vet[i])
             i+=1
-    def removeEnd(self):
+            
+    def removerFim(self):
         self.quant-=1
-    def itsFull(self):
-        return self.quant==5
-    def itsEmpty(self):
-        return self.quant==0
-    def getFirst(self):
-        return self.lista[0]
-    def getLast(self):
-        return self.lista[self.quant-1]
-    def inserirInicio(self,valor):
-        self.lista[0] == valor
-    def removerInicio(self,valor):
-        self.quant-=1
+
     def getPrim(self):
-        return self.list[0]
+        return self.vet[0]
+
     def getUlt(self):
-        return self.lista[self.quant-1] 
-    def InsertStart(self,value):
+        return self.vet[self.quant-1]
+
+    def inserirInicio(self,valor):
         i=self.quant
         while i >0:
-            self.list[i]=self.list[i-1]
+            self.vet[i]=self.vet[i-1]
             i-=1
-        self.list[0]=value
+        self.vet[0]=valor
         self.quant+=1
-    def RemoveStart(self):
+
+    def removerInicio(self):
         i=0
         while i<self.quant-1:
-            self.list[i]=self.list[i+1]
+            self.vet[i]=self.vet[i+1]
             i+=1
         self.quant-=1
-    def insertAfter(self,val1,val2):
+
+    def inserirApos(self,valor1,valor2):
         i=0
         while i < self.quant:
-            if self.list[i]==val2:
-                j=self.quant
-                while j>i+1:
-                    self.list[j]=self.list[j-1]
-                    j-=1
-                self.list[j]=val1
-                self.quant+=1
-            i+=1
-    def inserirAntes(self,valor1, valor2):
-        i=0
-        j=self.quant
-        while i < self.quant:
-            if self.lista[i] == valor2:
-                while j > i:
-                    self.lista[j] = self.lista[j-1]
-                    j-=1
-                self.lista[j]=valor1
+            if self.vet[i]==valor1:
+                aux = 0
+                aux=self.vet[i]
+                self.vet[i+1]=valor2
                 self.quant+=1
             i+=1
 
-    def substituir(self,val1,val2):
+    def inserirAposDet(self,valor1,valor2):
         i=0
         while i < self.quant:
-            if self.list[i]==val1:
-                self.list[i]=val2
+            if self.vet[i]==valor1:
+                j=self.quant
+                while j>i+1:
+                    self.vet[j]=self.vet[j-1]
+                    j-=1
+                self.vet[j]=valor2
+                self.quant+=1
             i+=1
-    def substituirEcontar(self,val1,val2):
-        i=0
-        cont=0
-        while i < self.quant:
-            if self.list[i]==val1:
-                self.list[i]=val2
-                cont+=1
-            i+=1
-        return cont
+
+    def limparLista(self):
+        self.quant=0
+        
+        
